b7d119e62abc26ce029ad8c26d5ba2b255a074ee
Roxanne Franco <roxannefranco10@gmail.com>
2023-02-02 00:02:41 +0100

    Improved file structure and config

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import '../css/app.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1 class="bg-red-500">Hello Vite!</h1>
+  </div>
+`
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
+import { resolve } from 'path'
 
 export default defineConfig({
-  // config options
-  plugins: [eslint()]
+  // Config options
+  plugins: [eslint()],
+  server: { port: 8000 },
+  // Allows better structure inside src folder
+  root: 'src',
+  build: {
+    // Config to build the necessary pages
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        login: resolve(__dirname, 'src/login.html')
+      }
+    }
+  }
 })
