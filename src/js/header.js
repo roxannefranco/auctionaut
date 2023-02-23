b3364a8d6eb57a2168019779adc89cf112a16d9d
Roxanne Franco <roxannefranco10@gmail.com>
2023-02-23 22:59:11 +0100

    Nav of public landing page

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,24 @@
+import { search } from './icons'
+import logo from './logo'
+
 // selecting header element
 const header = document.querySelector('header')
 
 // insert content into html header
-header.innerHTML = `<h1 class="bg-red-500">hello</h1>`
+header.innerHTML = `
+    <div class="center-content">
+        <div class="flex">
+            <div class="flex items-center">
+                ${logo}
+                <div class="relative ml-6">
+                    <input id="search" class="search-input" placeholder="Search" />
+                    <span class="search-icon">${search}</span>
+                </div>
+            </div>
+            <div class="flex items-center">
+                <a>Sign up</a>
+                <a class="register-button">Register</a>
+            </div>
+        </div>
+    </div>
+`
