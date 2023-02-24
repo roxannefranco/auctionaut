4e107be9f0c3cc3201ca8d9d37f517930c387a21
Roxanne Franco <roxannefranco10@gmail.com>
2023-02-24 21:51:03 +0100

    Unauthenticated header and responsive

--- src/js/header.js
+++ src/js/header.js
@@ -7,17 +7,17 @@ const header = document.querySelector('header')
 // insert content into html header
 header.innerHTML = `
     <div class="center-content">
-        <div class="flex">
+        <div class="flex items-center">
             <div class="flex items-center">
-                ${logo}
-                <div class="relative ml-6">
+                <a class="cursor-pointer"href="#">${logo}</a>
+                <div class="relative ml-2 sm:ml-6 max-w-[165px] sm:max-w-[240px]">
                     <input id="search" class="search-input" placeholder="Search" />
                     <span class="search-icon">${search}</span>
                 </div>
             </div>
-            <div class="flex items-center">
-                <a>Sign up</a>
-                <a class="register-button">Register</a>
+            <div class="flex items-center ml-auto">
+                <a class="btn btn-secondary">Sign in</a>
+                <a href="#" class="btn btn-primary">Register</a>
             </div>
         </div>
     </div>
