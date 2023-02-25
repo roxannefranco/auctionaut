ee657bd8436a12a85e4b72b0aa0121aa3f088384
Roxanne Franco <roxannefranco10@gmail.com>
2023-02-25 16:48:10 +0100

    Listings inserted into HTML

--- src/js/api/getListings.mjs
+++ src/js/api/getListings.mjs
@@ -4,11 +4,16 @@ import { apiUrl } from '../config.mjs'
  * Get listings from tag 'gamernaut'
  */
 export default async function getListings() {
-  const response = await fetch(`${apiUrl}/listings?_tag=gamernaut`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  const data = await response.json()
+  try {
+    const response = await fetch(`${apiUrl}/listings?_tag=gamernaut`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    return data
+  } catch (e) {
+    console.log(e)
+  }
 }
--- src/js/pages/index.mjs
+++ src/js/pages/index.mjs
@@ -1,5 +1,19 @@
 import '../../css/app.css'
 import getListings from '../api/getListings.mjs'
 
-// Get listings from API
-getListings()
+async function loadAndInsertListings() {
+  // Get listings from API
+  const listings = await getListings()
+
+  // insert listings into HTML
+  const listingsContainer = document.querySelector('#listings-container')
+
+  console.log(listings)
+  listings.map(function (listing) {
+    listingsContainer.innerHTML += `
+        <div>${listing.title}</div>
+    `
+  })
+}
+
+loadAndInsertListings()
