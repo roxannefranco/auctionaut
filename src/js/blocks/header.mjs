a047908e30b1b0a9583d86e896a805e0117f49db
Roxanne Franco <roxannefranco10@gmail.com>
2023-02-27 18:46:40 +0100

    Small adjustments

--- src/js/blocks/header.mjs
+++ src/js/blocks/header.mjs
@@ -10,14 +10,14 @@ header.innerHTML = `
         <div class="flex items-center">
             <div class="flex items-center">
                 <a class="cursor-pointer"href="#">${logo}</a>
-                <div class="relative ml-2 sm:ml-6 max-w-[165px] sm:max-w-[240px]">
+                <div class="search-input-container">
                     <input id="search" class="search-input" placeholder="Search" />
                     <span class="search-icon">${search}</span>
                 </div>
             </div>
             <div class="user-actions">
                 <a href="login.html" class="btn btn-secondary">Sign in</a>
-                <a href="#" class="btn btn-primary">Register</a>
+                <a href="register.html" class="btn btn-primary">Register</a>
             </div>
         </div>
     </div>
